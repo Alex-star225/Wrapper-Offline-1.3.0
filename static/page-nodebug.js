64df6fbe695fcf990b72dce6bcd414197f3aa307
Joseph The Animator <73652344+josephcrosmanplays532@users.noreply.github.com>
2021-05-04 17:14:49 -0400

    Initial commit

---
+++ static/page-nodebug.js
@@ -0,0 +1,240 @@
+const fUtil = require("../misc/file");
+const stuff = require("./info");
+const http = require("http");
+
+function toAttrString(table) {
+	return typeof table == "object"
+		? Object.keys(table)
+				.filter((key) => table[key] !== null)
+				.map((key) => `${encodeURIComponent(key)}=${encodeURIComponent(table[key])}`)
+				.join("&")
+		: table.replace(/"/g, '\\"');
+}
+function toParamString(table) {
+	return Object.keys(table)
+		.map((key) => `<param name="${key}" value="${toAttrString(table[key])}">`)
+		.join(" ");
+}
+function toObjectString(attrs, params) {
+	return `<object id="obj" ${Object.keys(attrs)
+		.map((key) => `${key}="${attrs[key].replace(/"/g, '\\"')}"`)
+		.join(" ")}>${toParamString(params)}</object>`;
+}
+
+/**
+ * @param {http.IncomingMessage} req
+ * @param {http.ServerResponse} res
+ * @param {import("url").UrlWithParsedQuery} url
+ * @returns {boolean}
+ */
+module.exports = function (req, res, url) {
+	if (req.method != "GET") return;
+	const query = url.query;
+
+	var attrs, params, title;
+	switch (url.pathname) {
+		case "/cc": {
+			title = "Character Creator";
+			attrs = {
+				data: process.env.SWF_URL + "/cc.swf", // data: 'cc.swf',
+				type: "application/x-shockwave-flash",
+				id: "char_creator",
+				width: "100%",
+				height: "100%",
+				style:'display:block;margin-left:auto;margin-right:auto;',
+			};
+			params = {
+				flashvars: {
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+					original_asset_id: query["id"] || null,
+					themeId: "business",
+					ut: 30,
+					bs: "default",
+					appCode: "go",
+					page: "",
+					siteId: "go",
+					m_mode: "school",
+					isLogin: "Y",
+					isEmbed: 1,
+					ctc: "go",
+					tlang: "en_US",
+                                        nextUrl: "../cc_browser",
+				},
+				allowScriptAccess: "always",
+				movie: process.env.SWF_URL + "/cc.swf", // 'http://localhost/cc.swf'
+			};
+			break;
+		}
+
+		case "/cc_browser": {
+			title = "CC Browser";
+			attrs = {
+				data: process.env.SWF_URL + "/cc_browser.swf", // data: 'cc_browser.swf',
+				type: "application/x-shockwave-flash",
+				id: "char_creator",
+				width: "100%",
+				height: "100%",
+				style:'display:block;margin-left:auto;margin-right:auto;',
+			};
+			params = {
+				flashvars: {
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+					original_asset_id: query["id"] || null,
+					themeId: "family",
+					ut: 30,
+					appCode: "go",
+					page: "",
+					siteId: "go",
+					m_mode: "school",
+					isLogin: "Y",
+                                        retut: 1,
+                                        goteam_draft_only: 1,
+					isEmbed: 1,
+					ctc: "go",
+					tlang: "en_US",
+					lid: 13,
+				},
+				allowScriptAccess: "always",
+				movie: process.env.SWF_URL + "/cc_browser.swf", // 'http://localhost/cc_browser.swf'
+			};
+			break;
+		}
+
+		case "/go_full": {
+			let presave =
+				query.movieId && query.movieId.startsWith("m")
+					? query.movieId
+					: `m-${fUtil[query.noAutosave ? "getNextFileId" : "fillNextFileId"]("movie-", ".xml")}`;
+			title = "Video Editor";
+			attrs = {
+				data: process.env.SWF_URL + "/go_full.swf",
+				type: "application/x-shockwave-flash",
+				width: "100%",
+				height: "100%",
+			};
+			params = {
+				flashvars: {
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					isEmbed: 1,
+					ctc: "go",
+					ut: 50,
+					bs: "default",
+					appCode: "go",
+					page: "",
+					siteId: "go",
+					lid: 13,
+					isLogin: "Y",
+					retut: 1,
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+					themeId: "custom",
+					tlang: "en_US",
+					presaveId: presave,
+					goteam_draft_only: 1,
+					isWide: 1,
+					collab: 0,
+					nextUrl: "../pages/html/list.html",
+				},
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
+			};
+			break;
+		}
+
+		case "/player": {
+			title = "Player";
+			attrs = {
+				data: process.env.SWF_URL + "/player.swf",
+				type: "application/x-shockwave-flash",
+				width: "100%",
+				height: "100%",
+			};
+			params = {
+				flashvars: {
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					ut: 30,
+					autostart: 1,
+					isWide: 1,
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+				},
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
+			};
+			break;
+		}
+
+		case "/recordWindow": {
+			title = "Record Window";
+			attrs = {
+				data: process.env.SWF_URL + "/player.swf",
+				type: "application/x-shockwave-flash",
+				width: "100%",
+				height: "100%",
+				quality: "medium",
+			};
+			params = {
+				flashvars: {
+					apiserver: "/",
+					storePath: process.env.STORE_URL + "/<store>",
+					ut: 30,
+					autostart: 0,
+					isWide: 1,
+					clientThemePath: process.env.CLIENT_URL + "/<client_theme>",
+				},
+				allowScriptAccess: "always",
+				allowFullScreen: "true",
+			};
+			break;
+		}
+
+		default:
+			return;
+	}
+	res.setHeader("Content-Type", "text/html; charset=UTF-8");
+	Object.assign(params.flashvars, query);
+	res.end(`
+	<head>
+		<script>
+			document.title='${title}',flashvars=${JSON.stringify(params.flashvars)}
+		</script>
+		<script>
+			if(window.location.pathname == '/player') {
+				function hideHeader() {
+					document.getElementById("header").style.display = "none";
+				}
+			} else if(window.location.pathname == '/recordWindow') {
+				function hideHeader() {
+					document.getElementById("header").style.display = "none";
+				}
+			} else if(window.location.pathname == '/go_full') {
+				function hideHeader() {
+					document.getElementById("header").style.display = "none";
+				}
+			}
+		</script>
+		<link rel="stylesheet" type="text/css" href="/pages/css/modern-normalize.css">
+		<link rel="stylesheet" type="text/css" href="/pages/css/global.css">
+		<style>
+			body {
+				background: #eee;
+			}
+		</style>
+	</head>
+	
+	<header id="header">
+		<a href="/"><h1 style="margin:0"><img id="logo" src="/pages/assets/logo.png" alt="Wrapper: Offline"/></h1>
+		<nav id="headbuttons">
+			<a class="button_small" onclick="document.getElementById('file').click()">UPLOAD A MOVIE</a>
+			<a href="/pages/html/create.html" class="button_big">CREATE</a>
+		</nav>
+	</header>
+	
+	<body style="margin:0px" onload="hideHeader()">${toObjectString(attrs, params)
+		}</body>${stuff.pages[url.pathname] || ''}`)
+	return true;
+};
